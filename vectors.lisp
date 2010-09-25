2d7c359579f1fb0b122c6b740955c81116673ab4
Frédéric Jolliton <git@frederic.jolliton.com>
2010-09-25 14:27:47 +0200

    update-state: handle nested lists.

--- vectors.lisp
+++ vectors.lisp
@@ -18,23 +18,25 @@
 (in-package #:net.tuxee.vectors)
 
 (defun update-state (state paths)
-  (dolist (path (if (listp paths) paths (list paths)))
-    (let ((iterator (path-iterator-segmented path)))
-      (multiple-value-bind (i1 k1 e1) (path-iterator-next iterator)
-        (declare (ignore i1))
-        (when (and k1 (not e1))
-          ;; at least 2 knots
-          (let ((first-knot k1))
-            (loop
-               (multiple-value-bind (i2 k2 e2) (path-iterator-next iterator)
-                 (declare (ignore i2))
-                 (line-f state
-                         (point-x k1) (point-y k1)
-                         (point-x k2) (point-y k2))
-                 (setf k1 k2)
-                 (when e2
-                   (return))))
-            (line-f state
-                    (point-x k1) (point-y k1)
-                    (point-x first-knot) (point-y first-knot)))))))
+  (if (listp paths)
+      (dolist (path paths)
+        (update-state state path))
+      (let ((iterator (path-iterator-segmented paths)))
+        (multiple-value-bind (i1 k1 e1) (path-iterator-next iterator)
+          (declare (ignore i1))
+          (when (and k1 (not e1))
+            ;; at least 2 knots
+            (let ((first-knot k1))
+              (loop
+                 (multiple-value-bind (i2 k2 e2) (path-iterator-next iterator)
+                   (declare (ignore i2))
+                   (line-f state
+                           (point-x k1) (point-y k1)
+                           (point-x k2) (point-y k2))
+                   (setf k1 k2)
+                   (when e2
+                     (return))))
+              (line-f state
+                      (point-x k1) (point-y k1)
+                      (point-x first-knot) (point-y first-knot)))))))
   state)
